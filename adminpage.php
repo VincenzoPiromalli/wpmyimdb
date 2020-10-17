6e933569a57630c8676df08c095ed8056ba27623
Vincenzo Piromalli <vpiromalli74@gmail.com>
2020-10-17 21:52:04 +0200

    Add files via upload

---
+++ adminpage.php
@@ -0,0 +1,16 @@
+<div class="wrap">
+<div id="welcome-panel" class="welcome-panel">
+<div class="welcome-panel-content">
+<h2>WP MyIMDB</h2>
+<p class="about-description">Thank you for purchasing the plugin.</p>
+<div class="welcome-panel-column-container">
+<h3>Get Started</h3>
+<ol>
+<li>Copy the file <code>sample.php</code> (located in plugin folder) in your theme folder. </li>
+<li>Open the <code>single.php</code> file of your theme and and right after <code class="html"><span class="nt"><</span><span class="na">?</span><span class="s">php</span> the_title(); <span class="nt">?</span><span class="na">></span></code> paste this code <code class="html"><span class="nt"><</span><span class="na">?</span><span class="s">php</span> get_template_part('sample'); <span class="nt">?</span><span class="na">></span></code> <a href="https://prnt.sc/uci1ah" target="_blank">example</a></li>
+<li>For any questions please contact me at <a href="https://sellix.io/fr0zen/contact" target="_blank">this address</a></li>
+</ol>
+</div>
+</div>
+</div>
+</div>
